254f8385c11af4d595b7df4ff5a16042de2c1c80
Luis Carlos <luiscarlossantos.lcs.16@gmail.com>
2022-06-27 16:39:28 -0300

    criando cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,8 +12,206 @@
                 @endif
                 </ul>
             </div>
-            <div class="col-10">
-                <h3>Novidades que chegaram para você</h3>
+            <div class="col-10 container-fluid novidades">
+                <div class="titulo">
+                    <h3>Novidades que chegaram para você</h3>
+                </div>
+                <div class="itens row ms-3 mt-4">
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-2">
+                        <div class="card mb-3">
+                            <div class="card-img p-1">
+                                <img src="imagens/1075.jpg" alt="image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <div class="d-flex flex-row align-items-end">
+                                        <span>FOGÃO MÔNACO 5 BOCAS ATLAS</span>
+                                    </div>
+                                </h5>
+                                <a href="" class="card-link">Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                    
+                </div>
             </div>
         </div>
     </main>
